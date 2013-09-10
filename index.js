109224f57933d9e7c33fe45e30d3659114e8a934
Carrington <daniel@danielingraham.com>
2013-09-10 22:26:55 +0000

    Just getting things started.

---
+++ index.js
@@ -0,0 +1,8 @@
+var 	async = require('async'),
+	_ = require('underscore'),
+	request = require('request'),
+	waterline = require('waterline');
+
+exports = module.exports = Obscura;
+
+Obscura.filterContent
