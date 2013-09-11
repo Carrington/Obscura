caae41ac76728f6962d926ee6ee807abc859c754
Carrington <daniel@danielingraham.com>
2013-09-11 04:14:57 +0000

    Minor edits.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var 	async = require('async'),
 	_ = require('underscore'),
 	request = require('request'),
-	waterline = require('waterline');
+	cheerio = require('lib/cheerio');
 
 exports = module.exports = Obscura;
 
-Obscura.filterContent
+
+//Expects filters to be an array, content to be a string
+Obscura.filterContent(filters, content, cb) {
+	groups = _.groupBy(filters, 'filterType');
+	for (var group in groups) {
+		if (this.hasOwnProperty(group)) {
+			content = this[group](groups[group], content);
+		}
+	}
+	cb(content);
+}
+
+Obscura.censorGroup(censorClasses, content, mode='html') {
+	for (var censorClass in censorClasses) {
+		$ = cheerio.load(content);
+		$('.' + censorClass).text('');
+	}
+}
