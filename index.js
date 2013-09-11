005b55138a855fbedabecf3056b61dc97bdf2d3d
Carrington <daniel@danielingraham.com>
2013-09-11 15:55:44 +0000

    replaceString function

--- index.js
+++ index.js
@@ -7,19 +7,29 @@ exports = module.exports = Obscura;
 
 
 //Expects filters to be an array, content to be a string
-Obscura.filterContent(filters, content, cb) {
+Obscura.filterContent(filters, content, mode, cb) {
+	mode = (typeof mode === "undefined") ? "html" : mode;
 	groups = _.groupBy(filters, 'filterType');
 	for (var group in groups) {
 		if (this.hasOwnProperty(group)) {
-			content = this[group](groups[group], content);
+			content = this[group](groups[group], content, mode);
 		}
 	}
 	cb(content);
 }
 
-Obscura.censorGroup(censorClasses, content, mode='html') {
+Obscura.censorGroup(censorClasses, content, mode) {
 	for (var censorClass in censorClasses) {
 		$ = cheerio.load(content);
 		$('.' + censorClass).text('');
 	}
 }
+
+Obscura.replaceString(replaceRules, content, mode) {
+	for (var rule in replaceRules) {
+		$ = cheerio.load(content);
+		$("#" + rule['target']).text(rule['replacement']);
+	}
+}
+
+
